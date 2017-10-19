6a69c789f643f13c79b6c56432673c5ee1f66d06
Katy Carr <khcarr9@gmail.com>
2017-10-19 23:04:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments) {
   var newArray = [];
-  for(i=0;i<musicians.length;i++) {
+  for(var i=0;i<musicians.length;i++) {
     string = musicians[0]+" plays "+instruments[i];
     newArray.push(string);
     return newArray;
